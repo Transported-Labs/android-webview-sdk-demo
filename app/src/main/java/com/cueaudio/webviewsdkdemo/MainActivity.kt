6ba1daa4f10ec7381fb3c1279d1d5f87890452f0
Alexander Mokrushin <amokrushin@transportedlabs.com>
2023-09-21 20:45:17 +0300

    Added support of Mozilla GeckoView.

--- app/src/main/java/com/cueaudio/webviewsdkdemo/MainActivity.kt
+++ app/src/main/java/com/cueaudio/webviewsdkdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.Toast
 import com.cueaudio.webviewsdk.InvalidUrlError
 import com.cueaudio.webviewsdk.WebViewController
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val webViewController = WebViewController(this)
         val urlEditText = findViewById<EditText>(R.id.urlEditText)
-        val openInBrowserButton = findViewById<Button>(R.id.openInBrowserButton)
+        val openInBrowserButton = findViewById<Button>(R.id.openInChromeButton)
         openInBrowserButton.setOnClickListener {
             val url = urlEditText.text.toString()
             if (url == "") {
@@ -24,6 +25,20 @@ class MainActivity : AppCompatActivity() {
             }
             try {
                 webViewController.openInChrome(url)
+            } catch (e: InvalidUrlError) {
+                // Show invalid URL error message
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+        val navigateGeckoButton = findViewById<Button>(R.id.navigateGeckoButton)
+        navigateGeckoButton.setOnClickListener {
+            val url = urlEditText.text.toString()
+            if (url == "") {
+                println("Empty URL is not allowed")
+                return@setOnClickListener
+            }
+            try {
+                webViewController.navigateToGecko(url)
             } catch (e: InvalidUrlError) {
                 // Show invalid URL error message
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
@@ -46,6 +61,17 @@ class MainActivity : AppCompatActivity() {
         val openFileButton = findViewById<Button>(R.id.openFileButton)
         openFileButton.setOnClickListener {
             webViewController.navigateTo("file:///android_asset/index.html")
+        }
+        val openFileGeckoButton = findViewById<Button>(R.id.openFileGeckoButton)
+
+        // Copy demo page to app's private storage
+        // This is required because there is no way to match web extension's content_scripts
+        // to resource://android/assets/... and jar:file://... URLs which are used for
+        // communication between page and native app (by web extension in-between).
+        assets.open("index.html").copyTo(File(filesDir, "index.html").outputStream())
+
+        openFileGeckoButton.setOnClickListener {
+            webViewController.navigateToGecko("file://${filesDir.absolutePath}/index.html")
         }
     }
 }
