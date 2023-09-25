7d8835b0d214fbb8e4323bc035e1b187d87ba4e8
Alexander Mokrushin <amokrushin@transportedlabs.com>
2023-09-25 17:12:27 +0300

    Removed support of Mozilla GeckoView.

--- app/src/main/java/com/cueaudio/webviewsdkdemo/MainActivity.kt
+++ app/src/main/java/com/cueaudio/webviewsdkdemo/MainActivity.kt
@@ -30,20 +30,6 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
         }
-        val navigateGeckoButton = findViewById<Button>(R.id.navigateGeckoButton)
-        navigateGeckoButton.setOnClickListener {
-            val url = urlEditText.text.toString()
-            if (url == "") {
-                println("Empty URL is not allowed")
-                return@setOnClickListener
-            }
-            try {
-                webViewController.navigateToGecko(url)
-            } catch (e: InvalidUrlError) {
-                // Show invalid URL error message
-                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-            }
-        }
         val navigateButton = findViewById<Button>(R.id.navigateButton)
         navigateButton.setOnClickListener {
             val url = urlEditText.text.toString()
@@ -61,17 +47,6 @@ class MainActivity : AppCompatActivity() {
         val openFileButton = findViewById<Button>(R.id.openFileButton)
         openFileButton.setOnClickListener {
             webViewController.navigateTo("file:///android_asset/index.html")
-        }
-        val openFileGeckoButton = findViewById<Button>(R.id.openFileGeckoButton)
-
-        // Copy demo page to app's private storage
-        // This is required because there is no way to match web extension's content_scripts
-        // to resource://android/assets/... and jar:file://... URLs which are used for
-        // communication between page and native app (by web extension in-between).
-        assets.open("index.html").copyTo(File(filesDir, "index.html").outputStream())
-
-        openFileGeckoButton.setOnClickListener {
-            webViewController.navigateToGecko("file://${filesDir.absolutePath}/index.html")
         }
     }
 }
