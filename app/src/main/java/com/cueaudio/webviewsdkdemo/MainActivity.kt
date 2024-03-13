2439a5b7bba73b76d57a18db6f7691bbe74f9ca1
Alexander Mokrushin <alex.mokrushin@gmail.com>
2024-03-13 18:20:00 +0300

    Added "Prefetch" and "Scan QR" features.

--- app/src/main/java/com/cueaudio/webviewsdkdemo/MainActivity.kt
+++ app/src/main/java/com/cueaudio/webviewsdkdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cueaudio.webviewsdkdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,14 +8,19 @@ import android.widget.EditText
 import android.widget.Toast
 import com.cueaudio.webviewsdk.InvalidUrlError
 import com.cueaudio.webviewsdk.WebViewController
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var urlEditText: EditText
+    private lateinit var navigateButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val webViewController = WebViewController(this)
-        val urlEditText = findViewById<EditText>(R.id.urlEditText)
+        urlEditText = findViewById(R.id.urlEditText)
         val openInBrowserButton = findViewById<Button>(R.id.openInChromeButton)
         openInBrowserButton.setOnClickListener {
             val url = urlEditText.text.toString()
@@ -29,7 +35,32 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
         }
-        val navigateButton = findViewById<Button>(R.id.navigateButton)
+        val scanQRButton = findViewById<Button>(R.id.scanQRButton)
+        scanQRButton.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            integrator.setBeepEnabled(false)
+            integrator.setOrientationLocked(true)
+            integrator.initiateScan()
+        }
+        val prefetchButton = findViewById<Button>(R.id.prefetchButton)
+        prefetchButton.setOnClickListener {
+            val urlString = urlEditText.text.toString()
+            val url = "${urlString}&preload=true"
+            if (url == "") {
+                println("Empty URL is not allowed")
+                return@setOnClickListener
+            }
+            try {
+                webViewController.prefetch(url) {
+                    prefetchButton.text = "Fetched:$it%"
+                }
+            } catch (e: InvalidUrlError) {
+                // Show invalid URL error message
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+        navigateButton = findViewById(R.id.navigateButton)
         navigateButton.setOnClickListener {
             val url = urlEditText.text.toString()
             if (url == "") {
@@ -46,6 +77,19 @@ class MainActivity : AppCompatActivity() {
         val openFileButton = findViewById<Button>(R.id.openFileButton)
         openFileButton.setOnClickListener {
             webViewController.navigateTo("file:///android_asset/index.html")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null && result.contents != null) {
+            val scannedData = result.contents
+            urlEditText.setText(scannedData)
+            if (scannedData.contains("https://")) {
+                navigateButton.performClick()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
